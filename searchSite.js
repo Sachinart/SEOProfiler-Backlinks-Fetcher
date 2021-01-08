ad97cfa7e3b790f4d7911ac9673cecb390589a95
Sachin Artani <sachinartani@yahoo.in>
2021-01-08 21:53:54 +0530

    Storing cookies to avoid multiple session creation

--- searchSite.js
+++ searchSite.js
@@ -1,15 +1,32 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 const fakeUa = require('fake-useragent');
+const fs = require('fs');
 
 const searchSite = async (w) => {
     const browser = await puppeteer.launch();
 
     const page = await browser.newPage();
-
-	// set user agent (override the default headless User Agent)
+	
+    let cookiesFilePath = "cookies.json";
+ 
+    // set user agent (override the default headless User Agent)
     await page.setUserAgent(fakeUa());
 
+
+	const previousSession = fs.existsSync(cookiesFilePath)
+	if (previousSession) {
+	  // If file exist load the cookies
+	  const cookiesString = fs.readFileSync(cookiesFilePath);
+	  const parsedCookies = JSON.parse(cookiesString);
+	  if (parsedCookies.length !== 0) {
+		for (let cookie of parsedCookies) {
+		  await page.setCookie(cookie)
+		}
+		console.log('Session has been loaded in the browser');
+	  }
+	}
+	
     await page.goto('https://www.seoprofiler.com/lp/links?pid={YOUR PID HERE}&q='+ w + '&num=100');
 
 	// get the User Agent on the context of Puppeteer
@@ -18,15 +35,29 @@ const searchSite = async (w) => {
 	// If everything correct then no 'HeadlessChrome' sub string on userAgent
 	//console.log(userAgent);
 	
-    //Finds input elements to enter username and password
-    await page.type('input[name="username"]', '{YOUR USERNAME OR EMAIL ADDRESS}');
-    await page.type('input[name="password"]', '{YOUR PASSWORD}');
-	
-    //Finds an button to submit the form, after so it executes .click() DOM Method
-    await page.$eval('button[name="button"]', button => button.click());
+    if (!previousSession) {
+		//Finds input elements to enter username and password
+		await page.type('input[name="username"]', '{SEOPROFILER USERNAME}');
+		await page.type('input[name="password"]', '{SEOPROFILER PASSWORD}');
+		
+		//Finds an button to submit the form, after so it executes .click() DOM Method
+		await page.$eval('button[name="button"]', button => button.click());
+
+		
+		// Save Session Cookies
+		const cookiesObject = await page.cookies()
+		// Write cookies to temp file to be used in other profile pages
+		fs.writeFile(cookiesFilePath, JSON.stringify(cookiesObject),
+		 function(err) { 
+		  if (err) {
+		  console.log('The file could not be written.', err)
+		  }
+		  console.log('Session has been successfully saved')
+		});
+	}
 	
 	// take the screenshot to check if correct page is loaded
-	await page.screenshot({path: 'example.png'});
+	await page.screenshot({path: 'page.png'});
 
 	// if the selector holding the data exists on load
 	if ((await page.$('div[id="lpt4"]')) !== null) {
